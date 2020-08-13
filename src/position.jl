96a730619e7e944ca1f052a9d028cad59806ebf6
Jungsoo Kim <urlicht@users.noreply.github.com>
2020-08-13 12:08:05 -0400

    Update position.jl
    function doc

--- src/position.jl
+++ src/position.jl
@@ -1,11 +1,18 @@
+"""
+    set_zero(s::SerialPort)
+Sets the current x, y position to 0, 0
+"""
 function set_zero(s::SerialPort)
-    # example return_str: ":A 454902 -98089 "
     sp_return = write(s, "HERE X=0 Y=0\r")
     write_check_error(s, sp_return)
 
     nothing
 end
 
+"""
+    get_position(s::SerialPort)
+Returns the current x, y position (Int)
+"""
 function get_position(s::SerialPort)
     # example return_str: ":A 454902 -98089 "
     sp_return = write(s, "WHERE X Y\r")
@@ -19,6 +26,10 @@ function get_position(s::SerialPort)
     end
 end
 
+"""
+    move_relative(s::SerialPort, dx::Int, dy::Int)
+Move (relative) the stage position
+"""
 function move_relative(s::SerialPort, dx::Int, dy::Int)
     sp_return = write(s, "MOVEI X=$dx Y=$dy\r")
     write_check_error(s, sp_return)
